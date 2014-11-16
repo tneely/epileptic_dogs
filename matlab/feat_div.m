8ec5ced3af067faaa2652ed0d9f5dce93ce9c75c
Taylor Neely <tneely417@gmail.com>
2014-11-15 22:46:32 -0500

    feat_div typo fix

--- matlab/feat_div.m
+++ matlab/feat_div.m
@@ -17,7 +17,7 @@ function feats = feat_div(X,freq,division,windows,overlap,spec_edge,sig_pow,avg_
     for i=1:division
         fprintf('Entering division %d: \n',i)
         ind = 1;
-        win_feats = []
+        win_feats = [];
         for j = 1:windows
             fprintf('Window %d... \n',j)
             win_feats = [win_feats, features(X(:,ind:ind+pts_per_win,(m*(i-1)+1):(m*i)),freq,spec_edge,sig_pow,avg_pow)];
