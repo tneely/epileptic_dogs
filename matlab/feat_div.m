8b5a4158803144cd9522c622858677499677ec5c
Taylor Neely <tneely417@gmail.com>
2014-11-15 23:14:43 -0500

    feat_div restored to original purpose, feat_win created for windowing

--- matlab/feat_div.m
+++ matlab/feat_div.m
@@ -3,34 +3,16 @@
     memory contraints for feature generation.
 %}
 
-function feats = feat_div(X,freq,division,windows,overlap,spec_edge,sig_pow,avg_pow)
+function feats = feat_div(X,freq,division,spec_edge,sig_pow,avg_pow)
     
-    tic
-    
-    %init and division
     feats = [];
     m = size(X,3) / division;
-    pts = size(X,2);
-    pts_per_win = floor(pts/(windows*(1-overlap)));
-    step = floor(pts_per_win * (1-overlap));
 
     for i=1:division
         fprintf('Entering division %d: \n',i)
-        ind = 1;
-        win_feats = [];
-        for j = 1:windows
-            fprintf('Window %d... \n',j)
-            win_feats = [win_feats, features(X(:,ind:ind+pts_per_win,(m*(i-1)+1):(m*i)),freq,spec_edge,sig_pow,avg_pow)];
-            ind = ind + step;
-        end
-        
-        feats = [feats, win_feats];
-        
+        feats = [feats; features(X(:,:,(m*(i-1)+1):(m*i)),freq,spec_edge,sig_pow,avg_pow)];
         fprintf('Division %d finished. \n',i)        
     end
-    
-    fprintf('\n');
-    toc
     
 end
         
---
+++ matlab/feat_win.m
@@ -0,0 +1,28 @@
+%{
+    Breaks up input segments into N pieces to fit 
+    memory contraints for feature generation.
+%}
+
+function feats = feat_win(X,freq,windows,overlap,spec_edge,sig_pow,avg_pow)
+    
+    tic
+    
+    %init and window division
+    feats = [];
+    pts = size(X,2);
+    pts_per_win = floor(pts/(windows*(1-overlap)));
+    step = floor(pts_per_win * (1-overlap));
+    ind = 1;
+    
+    for i = 1:windows
+        fprintf('Starting window %d... \n',i)
+        feats = [feats, features(X(:,ind:ind+pts_per_win,:),freq,spec_edge,sig_pow,avg_pow)];
+        ind = ind + step;
+        fprintf('Window %d finished. \n',i) 
+        fprintf('\n')
+    end     
+
+    toc
+    
+end
+        
