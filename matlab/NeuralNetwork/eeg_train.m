92e583e1b221310e96c512d3e7c0a55caafba480
Taylor Neely <tneely417@gmail.com>
2014-11-16 20:10:01 -0500

    whatevers here

--- matlab/NeuralNetwork/eeg_train.m
+++ matlab/NeuralNetwork/eeg_train.m
@@ -15,13 +15,16 @@
 function error = eeg_train(X, Y, trials, a, hidden)
 
     net = FeedForwardNetwork();
-    net.learning_rate = a;
     
     [numSegs, numFeats] = size(X);
     
+    pre_ratio = sum(Y)/numSegs;
+    inter_rate = a*(pre_ratio);
+    pre_rate = a*(1-pre_ratio);
+    
     net.addInputLayer(numFeats,numFeats);
     net.addHiddenLayer(numFeats, hidden);
-    net.addOutputLayer(hidden,1,0,0);
+    net.addOutputLayer(hidden,1,0,1);
     
     error = zeros(trials,1);
     
@@ -29,9 +32,19 @@ function error = eeg_train(X, Y, trials, a, hidden)
         
         fprintf('Starting trial %d... ', trial)
         
+        rand('twister', 0);
+        indPerm = randperm(numSegs);
+        
         accum_error = 0;
         
-        for i = 1:numSegs
+        for i = indPerm
+            
+            if Y(1,i)
+                net.learning_rate = pre_rate;
+            else
+                net.learning_rate = inter_rate;
+            end
+            
             accum_error = accum_error + backpropagation(net, X(i,:), Y(1,i));
         end
         
