aedd5d747d850f524e59fd2bf520f4c9d43489a0
Taylor Neely <tneely417@gmail.com>
2014-11-17 12:35:43 -0500

    new results

--- matlab/overnight.m
+++ matlab/overnight.m
@@ -1,5 +1,5 @@
-[error1 , net1] = eeg_train(feats1_1w_0o,Y5,500,.3,0.85,0.05);
-[error2 , net2] = eeg_train(feats2_1w_0o,Y5,500,.3,0.85,0.05);
-[error3 , net3] = eeg_train(feats3_1w_0o,Y5,500,.3,0.85,0.05);
-[error4 , net4] = eeg_train(feats4_1w_0o,Y5,500,.3,0.85,0.05);
+[error1 , net1] = eeg_train(feats1_1w_0o,Y1,500,.3,0.85,0.05);
+[error2 , net2] = eeg_train(feats2_1w_0o,Y2,500,.3,0.85,0.05);
+[error3 , net3] = eeg_train(feats3_1w_0o,Y3,500,.3,0.85,0.05);
+[error4 , net4] = eeg_train(feats4_1w_0o,Y4,500,.3,0.85,0.05);
 [error5 , net5] = eeg_train(feats5_1w_0o,Y5,500,.3,0.85,0.05);
---
+++ matlab/overnight3.m
@@ -0,0 +1,5 @@
+[sensitivity1, fpr1] = cross_val(feats1_1w_0o,Y1,10,250);
+[sensitivity2, fpr2] = cross_val(feats2_1w_0o,Y2,10,250);
+[sensitivity3, fpr3] = cross_val(feats3_1w_0o,Y3,10,250);
+[sensitivity4, fpr4] = cross_val(feats4_1w_0o,Y4,10,250);
+[sensitivity5, fpr5] = cross_val(feats5_1w_0o,Y5,10,250);
