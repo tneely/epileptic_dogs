2d6aa40400dd837d5d9c8d1338e56caf205b285f
Taylor Neely <tneely417@gmail.com>
2014-11-16 23:12:08 -0500

    overnight script

--- matlab/overnight.m
+++ matlab/overnight.m
@@ -1,5 +1,5 @@
-[error1 , net1] = eeg_train(feats5_1w_0o,Y5,100,.3);
-[error5 , net5] = eeg_train(feats5_5w_0o,Y5,100,.3);
-[error10 , net10] = eeg_train(feats5_10w_0o,Y5,100,.3);
-[error20 , net20] = eeg_train(feats5_20w_0o,Y5,100,.3);
-[error30 , net30] = eeg_train(feats5_30w_0o,Y5,100,.3);
+[error1 , net1] = eeg_train(feats5_1w_0o,Y5,250,.3);
+[error5 , net5] = eeg_train(feats5_5w_0o,Y5,250,.3);
+[error10 , net10] = eeg_train(feats5_10w_0o,Y5,250,.3);
+[error20 , net20] = eeg_train(feats5_20w_0o,Y5,250,.3);
+[error30 , net30] = eeg_train(feats5_30w_0o,Y5,250,.3);
