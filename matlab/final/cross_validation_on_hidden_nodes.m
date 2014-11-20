1f40391d07db94a265dbcb8d8c0ae62e4658dfa1
JackFosterTerwilliger <jack.f.terwilliger.15@dartmouth.edu>
2014-11-20 13:04:46 -0500

    hid

--- matlab/final/cross_validation_on_hidden_nodes.m
+++ matlab/final/cross_validation_on_hidden_nodes.m
@@ -1,9 +1,9 @@
-[sensitivity_1pt0, fpr_1pt0] = cross_val_hidden_nodes(feats1_1w_0o, Y1, 10, 200, 1);
+[sensitivity_1pt0, fpr_1pt0] = cross_val_hidden_nodes(feats5_1w_0o, Y5, 10, 200, 1);
 
-[sensitivity_0pt75, fpr_0pt75] = cross_val_hidden_nodes(feats1_1w_0o, Y1, 10, 200, .75);
+[sensitivity_0pt75, fpr_0pt75] = cross_val_hidden_nodes(feats5_1w_0o, Y5, 10, 200, .75);
 
-[sensitivity_0pt5, fpr_0pt5] = cross_val_hidden_nodes(feats1_1w_0o, Y1, 10, 200, .5);
+[sensitivity_0pt5, fpr_0pt5] = cross_val_hidden_nodes(feats5_1w_0o, Y5, 10, 200, .5);
 
-[sensitivity_0pt3, fpr_0pt3] = cross_val_hidden_nodes(feats1_1w_0o, Y1, 10, 200, .3);
+[sensitivity_0pt3, fpr_0pt3] = cross_val_hidden_nodes(feats5_1w_0o, Y5, 10, 200, .3);
 
-[sensitivity_0pt25, fpr_0pt25] = cross_val_hidden_nodes(feats1_1w_0o, Y1, 10, 200, .25);
+[sensitivity_0pt25, fpr_0pt25] = cross_val_hidden_nodes(feats5_1w_0o, Y5, 10, 200, .25);
