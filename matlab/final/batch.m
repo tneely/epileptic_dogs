e00a71c6629ca2f11249400f152cd92ba9caf526
Taylor Neely <tneely417@gmail.com>
2014-11-21 12:24:57 -0500

    more data

--- matlab/final/batch.m
+++ matlab/final/batch.m
@@ -1,6 +1,3 @@
 %batch runs
-[sens1,fpr1] = cross_val(feats2_1w_0o, Y2, 10, 1000);
-[sens2,fpr2] = cross_val(feats2_1w_0o, Y2, 10, 1000);
-[sens3,fpr3] = cross_val(feats3_1w_0o, Y3, 10, 1000);
-[sens4,fpr4] = cross_val(feats4_1w_0o, Y4, 10, 1000);
-[sens5,fpr5] = cross_val(feats5_1w_0o, Y5, 10, 1000);
+error1_normal = eeg_train(feats1_1w_0o, Y1, 5000, 0.3, 0.85, 0.05);
+error1_no_m_d = eeg_train(feats1_1w_0o, Y1, 5000, 0.3, 0, 0);
